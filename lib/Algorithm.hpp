a6ec03458c2edbbbff231079f0b3f0f7e8bc3e71
giaan <ngonguyengiaan2k7@gmail.com>
2026-06-18 22:21:18 +0700

    update algorithm.hpp

--- lib/Algorithm.hpp
+++ lib/Algorithm.hpp
@@ -1,146 +1,238 @@
-#pragma once
 #ifndef ALGORITHM_HPP
 #define ALGORITHM_HPP
-#include <stdexcept>
+
 #include <cstddef>
 #include <functional>
+#include <utility>
 
 namespace lib {
+    // gia tri tra ve neu khong tim thay
+    inline constexpr std::size_t npos =
+        static_cast<std::size_t>(-1);
+
+    // ham swap: hoan doi gia tri cua 2 bien
     template <typename T>
     void swap(T& a, T& b) {
-        T tmp = a;
-        a = b;
-        b = tmp;
+        T temp = std::move(a);
+        a = std::move(b);
+        b = std::move(temp);
     }
-    
+
+    // LINEAR SEARCH
+    // ham linear_search: tim kiem tuan tu
     template <typename T>
-    size_t linear_search(const T a[], size_t n, const T& value) {
-        for (size_t i = 0; i < n; i++) {
-            if (a[i] == value) {
-                return i;
-            }
+    std::size_t linear_search(const T a[], std::size_t n, const T& value) {
+        if (a == nullptr) return npos;
+
+        for (std::size_t i = 0; i < n; ++i) {
+            if (a[i] == value) return i;
         }
-        return -1;
+        return npos;
     }
 
-    template <typename T>
-    size_t binary_search(const T a[], size_t n, const T& value) {
-        size_t l = 0;
-        size_t r = n - 1;
-        while (l <= r) {
-            size_t m = l + (r - l) / 2;
-            if (a[m] = value) return m;
-            else if (a[m] > value) r = m - 1;
-            else l = m + 1;
+    // BINARY SEARCH
+    // ham binary_search: tim kiem nhi phan, mang phai duoc sap xep truoc 
+    template <typename T, typename Comp = std::less<T>>
+    std::size_t binary_search(const T a[], std::size_t n, const T& value, Comp cmp = Comp{}) {
+        if (a == nullptr) return npos;
+        std::size_t left = 0;
+        std::size_t right = n;
+
+        while (left < right) {
+            const std::size_t mid =
+                left + (right - left) / 2;
+            if (cmp(a[mid], value)) left = mid + 1;
+            else if (cmp(value, a[mid])) right = mid;
+            else return mid;
         }
-        return -1;
+        return npos;
     }
 
+    // SELECTION SORT
+    // ham selection_sort: sap xep chon
     template <typename T, typename Comp = std::less<T>>
-    void selection_sort(T a[], size_t n, Comp cmp = std::less<T>()) {
-        for (size_t i = 0; i < n - 1; i++) {
-            size_t min_idx = i;
-            for (size_t j = i + 1; j < n; j++) {
-                if (cmp(a[j], a[min_idx])) min_idx = j;
-            }
-            swap(a[i], a[min_idx]);
-        }
-    }
+    void selection_sort(T a[], std::size_t n, Comp cmp = Comp{}) {
+        if (a == nullptr || n < 2) return;
 
-    template <typename T, typename Comp = std::less<T>>
-    void insertion_sort(T a[], size_t n, Comp cmp = std::less<T>()) {
-        for (size_t i = 1; i < n; i++) {
-            T key = a[i];
-            size_t j = i - 1;
-            while (j >= 0 && cmp(key, a[j])) {
-                a[j + 1] = a[j];
-                j--;
-            }
-            a[j + 1] = key;
-        }
-    } 
+        for (std::size_t i = 0; i + 1 < n; ++i) {
+            std::size_t minIdx = i;
 
-    template <typename T, typename Comp = std::less<T>>
-    void bubble_sort(T a[], size_t n, Comp cmp = std::less<T>()) {
-        for (size_t i = 0; i < n - 1; i++) {
-            for (size_t j = 0; j < n - i - 1; j++) {
-                if (cmp(a[j + 1], a[j])) swap(a[j + 1], a[j]);
-            }
+            for (std::size_t j = i + 1; j < n; ++j) 
+                if (cmp(a[j], a[minIdx])) minIdx = j;
+
+            if (minIdx != i) swap(a[i], a[minIdx]);
         }
     }
 
+    // INSERTION SORT
+    // ham insertion_sort: sap xep chen
     template <typename T, typename Comp = std::less<T>>
-    void heapify(T a[], size_t n, size_t i, Comp cmp = std::less<T>()) {
-        size_t largest = i;
-        size_t l = 2 * i + 1;
-        size_t r = 2 * i + 2;
-        if (l < n && cmp(a[largest], a[l])) largest = l;
-        if (r < n && cmp(a[largest], a[r])) largest = r;
-        if (largest != i) {
+    void insertion_sort(T a[], std::size_t n, Comp cmp = Comp{}) {
+        if (a == nullptr || n < 2) return;
+
+        for (std::size_t i = 1; i < n; ++i) {
+            T key = std::move(a[i]);
+            std::size_t j = i;
+
+            while (j > 0 && cmp(key, a[j - 1])) {
+                a[j] = std::move(a[j - 1]);
+                --j;
+            }
+
+            a[j] = std::move(key);
+        }
+    }
+
+    // BUBBLE SORT
+    // ham bubble_sort: sap xep noi bot
+    template <typename T, typename Comp = std::less<T>>
+    void bubble_sort(T a[], std::size_t n, Comp cmp = Comp{}) {
+        if (a == nullptr || n < 2) return;
+
+        for (std::size_t i = 0; i < n; ++i) {
+            bool swapped = false;
+
+            for (std::size_t j = 0; j < n - i - 1; ++j) {
+                if (cmp(a[j + 1], a[j])) {
+                    swap(a[j], a[j + 1]);
+                    swapped = true;
+                }
+            }
+
+            if (!swapped) break;
+        }
+    }
+
+    // HEAP SORT: sap xep vun dong
+    // ham heapify: vun dong mot cay con co goc la node i
+    template <typename T, typename Comp = std::less<T>>
+    void heapify(T a[], std::size_t n, std::size_t i, Comp cmp = Comp{}) {
+        if (a == nullptr || n < 2 || i >= n) return;
+
+        while (i < n / 2) {
+            std::size_t largest = i;
+
+            const std::size_t left = 2 * i + 1;
+            const std::size_t right = left + 1;
+
+            if (left < n && cmp(a[largest], a[left])) largest = left;
+
+            if (right < n && cmp(a[largest], a[right])) largest = right;
+
+            if (largest == i) return;
+
             swap(a[i], a[largest]);
-            heapify(a, n, largest, cmp);
+            i = largest;
         }
     }
 
+    // ham heap_sort: sap xep vun dong
     template <typename T, typename Comp = std::less<T>>
-    void heap_sort(T a[], size_t n, Comp cmp = std::less<T>()) {
-        for (size_t i = n / 2 - 1; i >= 0 ;i--) {
-            heapify(a, n, i, cmp);
-        }
-        for (size_t i = n - 1; i > 0; i--) {
+    void heap_sort(T a[], std::size_t n, Comp cmp = Comp{}) {
+        if (a == nullptr || n < 2) return;
+
+        for (std::size_t i = n / 2; i > 0; --i) heapify(a, n, i - 1, cmp);
+
+        for (std::size_t i = n - 1; i > 0; --i) {
             swap(a[0], a[i]);
             heapify(a, i, 0, cmp);
         }
     }
 
+    // MERGE SORT
+    // ham merge: gop 2 doan con da sap xep thanh 1 doan da sap xep
     template <typename T, typename Comp = std::less<T>>
-    void merge(T a[], size_t l, size_t m, size_t r, Comp cmp = std::less<T>()) {
-        size_t i = l;
-        size_t j = m + 1;
-        size_t k = 0;
-        T *res = new T[r - l + 1];
-        while (i <= m && j <= r) {
-            if (cmp(a[i], a[j])) res[k++] = a[i++];
-            else res[k++] = a[j++];
+    void merge(T a[], std::size_t left, std::size_t middle, std::size_t right, Comp cmp = Comp{}) {
+        if (a == nullptr || right == npos || left > middle || middle >= right) return;
+
+        const std::size_t size = right - left + 1;
+
+        T* temp = new T[size];
+
+        std::size_t i = left;
+        std::size_t j = middle + 1;
+        std::size_t k = 0;
+
+        try {
+            while (i <= middle && j <= right) {
+                if (!cmp(a[j], a[i])) {
+                    temp[k++] = a[i++];
+                } else {
+                    temp[k++] = a[j++];
+                }
+            }
+
+            while (i <= middle) {
+                temp[k++] = a[i++];
+            }
+
+            while (j <= right) {
+                temp[k++] = a[j++];
+            }
+
+            for (std::size_t x = 0; x < size; ++x) {
+                a[left + x] = std::move(temp[x]);
+            }
+        } catch (...) {
+            delete[] temp;
+            throw;
         }
-        while (i <= m) res[k++] = a[i++];
-        while (j <= r) res[k++] = a[j++];
-        for (size_t x = 0; x < r - l + 1; x++) a[l + x] = res[x];
-        delete[] res;
+
+        delete[] temp;
     }
 
+    // ham merge_sort: sap xep tron
     template <typename T, typename Comp = std::less<T>>
-    void merge_sort(T a[], size_t l, size_t r, Comp cmp = std::less<T>()) {
-        if (r - l + 1 <= 1) return;
-        size_t m = l + (r - l) / 2;
-        merge_sort(a, l, m, cmp);
-        merge_sort(a, m + 1, r, cmp);
-        merge(a, l, m, r, cmp);
+    void merge_sort(T a[], std::size_t left, std::size_t right, Comp cmp = Comp{}) {
+        if (a == nullptr || right == npos || left >= right) return;
+
+        const std::size_t mid = left + (right - left) / 2;
+
+        merge_sort(a, left, mid, cmp);
+        merge_sort(a, mid + 1, right, cmp);
+        merge(a, left, mid, right, cmp);
     }
 
+    // QUICK SORT
+    // ham hoare partition: phan hoach theo hoare, tra ve chi so cua phan tu cuoi cung cua doan con trai
     template <typename T, typename Comp = std::less<T>>
-    size_t partition(T a[], size_t l, size_t r, Comp cmp = std::less<T>()) {
-        T pivot = a[l];
-        size_t i = l - 1;
-        size_t j = r + 1;
+    std::size_t partition(T a[], std::size_t left, std::size_t right, Comp cmp = Comp{}) {
+        const T pivot = a[left + (right - left) / 2];
+
+        std::size_t i = left;
+        std::size_t j = right;
+
         while (true) {
-            do{
-                i++;
-            } while (cmp(a[i], pivot));
-            do {
-                j--;
-            } while (cmp(pivot, a[j]));     
+            while (cmp(a[i], pivot)) ++i;
+
+            while (cmp(pivot, a[j])) --j;
+
             if (i >= j) return j;
+
             swap(a[i], a[j]);
+
+            ++i;
+            --j;
         }
     }
 
+    // ham quick_sort: sap xep nhanh
     template <typename T, typename Comp = std::less<T>>
-    void quick_sort(T a[], size_t l, size_t r, Comp cmp = std::less<T>()) {
-        if (l < r) {
-            size_t pi = partition(a, l, r, cmp);
-            quick_sort(a, l, pi, cmp);
-            quick_sort(a, pi + 1, r, cmp);
+    void quick_sort(T a[], std::size_t left, std::size_t right, Comp cmp = Comp{}) {
+        if (a == nullptr || right == npos || left >= right) return;
+
+        while (left < right) {
+            const std::size_t pi = partition(a, left, right, cmp);
+
+            if (pi - left < right - pi) {
+                if (left < pi) quick_sort(a, left, pi, cmp); 
+
+                left = pi + 1;
+            } else {
+                if (pi + 1 < right) quick_sort(a, pi + 1, right, cmp);
+
+                right = pi;
+            }
         }
     }
 }
